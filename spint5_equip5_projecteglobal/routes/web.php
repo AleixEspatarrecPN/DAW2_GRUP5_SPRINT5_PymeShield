208045e0b0939537529b98f3aaab7b9d6f7ef511
JuliaK18 <92729717+JuliaK18@users.noreply.github.com>
2023-03-15 16:55:32 +0100

    View "restore" funcional amb modal de confirmació

--- spint5_equip5_projecteglobal/routes/web.php
+++ spint5_equip5_projecteglobal/routes/web.php
@@ -31,10 +31,11 @@ Route::get('/', HomeController::class, '__invoke');
 Route::get('/index', HomeController::class);
 
 // Restaurar
-Route::get('/restore ', function(){return view('restaurar');});
-Route::get('/restaurar',[RestoreController::class, 'devices']);
-//Route::get('/restaurar', [RestoreController::class, 'listar']);
-Route::get('/restaurar/list', [RestoreController::class, 'restaurar']);
+Route::get('/restore', function(){return view('restaurar');});
+Route::get('/restaurar',[RestoreController::class, 'devices'])->name('restaurar');
+Route::post('/restore/{id}', [RestoreController::class, 'restoreDevice'])->name('restaurar');
+Route::get('/restore/{id}', [RestoreController::class, 'getIdDevice'])->name('restaurar');
+
 
 //Mostrar Dispositivos
 Route::get('/devices', function(){ return view('listdevices');});
